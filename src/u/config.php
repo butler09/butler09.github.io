012dde03b00f9a14537c97ee9bb70d19eb1e374c
butler09 <author@accountant.com>
2017-06-04 17:37:52 +0100

    Update config.php

--- src/u/config.php
+++ src/u/config.php
@@ -2,20 +2,20 @@
 
 return array(
 	/* This is a secure key that only you should know, an added layer of security for the image upload */
-    'secure_key' => 'somerandomlongstringoftextforkey',
+    'secure_key' => 'author12303',
 
     /* This is the url your output will be, usually http://www.domain.com/u/, also going to this url will be the gallery page */
-    'output_url' => 'http://example.com/u/',
+    'output_url' => 'http://author.rest/i/',
 
     /* This is a redirect url if the script is accessed directly */
-    'redirect_url' => 'http://example.com/',
+    'redirect_url' => 'http://author.rest/',
 
     /* This is a list of IPs that can access the gallery page (Leave empty for universal access) */
     'allowed_ips' => array('192.168.0.0', '0.0.0.0'),
 
     /* Page title of the gallery page */
-    'page_title' => 'My Upload Site',
+    'page_title' => 'Gallery',
 
     /* Heading text at the top of the gallery page */
-    'heading_text' => 'Uploading Site',
+    'heading_text' => 'Gallery',
 );
