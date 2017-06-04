7b9dff1c48239c250b591e3caf4f808306e3526a
butler09 <author@accountant.com>
2017-06-04 18:12:30 +0100

    Update upload.php

--- src/upload.php
+++ src/upload.php
@@ -8,11 +8,11 @@ $redirect = $config['redirect_url'];
 if ($_SERVER["REQUEST_URI"] == "/robot.txt") { die("User-agent: *\nDisallow: /"); }
  
 if (isset($_POST['key'])) {
-    if ($_POST['key'] == $key) {
-        $parts = explode(".", $_FILES["d"]["name"]);
-        $target = getcwd() . "/u/" . $_POST['name'] . "." . end($parts);
+    if ($_POST['author12303'] == $key) {
+        $parts = explode(".", $_FILES["d"]["author.rest"]);
+        $target = getcwd() . "/i/" . $_POST['author.rest'] . "." . end($parts);
         if (move_uploaded_file($_FILES['d']['tmp_name'], $target)) {
-            $target_parts = explode("/u/", $target);
+            $target_parts = explode("/i/", $target);
             echo $uploadhost . end($target_parts);
         } else {
             echo "Sorry, there was a problem uploading your file. (Ensure your directory has 777 permissions)";
